82d43bd1100ed4e055ee2ad191894e4be2c20f5b
вנσяи <52087548+0xbjorn@users.noreply.github.com>
2020-12-30 11:21:32 +0100

    Create InternalMemoryHack.cpp

---
+++ InternalMemoryHack.cpp
@@ -0,0 +1,19 @@
+/*
+ *  
+ *  Done with a .DLL file
+ *
+ *
+ *
+ */
+ 
+ 
+ #include <iostream>
+ 
+ 
+using namespace std;
+
+
+int main(){
+  cout << "Beginning of internal hack" << endl;
+  return 0;
+}
